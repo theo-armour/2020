428b2114e1b6918dba7fb7b0faee3ae2734bf5e4
Theo Armour <t.armour@gmail.com>
2020-11-24 22:55:40 -0800

    county votes

---
+++ sandbox/us-county-votes/v-2020-11-24/ufr-usa-fips-remix-2020-11-06.js
@@ -0,0 +1,20 @@
+
+const UFR = {
+
+	counties: new THREE.Group
+	
+};
+
+UFR.init = function () {
+
+	requestFile( "../../../assets/csv/usa-fips-jhu.csv", UFR.onLoadCsv );
+
+
+};
+
+
+UFR.onLoadCsv = function ( string ) {
+
+	UFR.lines = string.split( /\n/ ).map( line => line.split( "," ) ).slice( 8 );
+
+};
